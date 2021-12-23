aa36ec0c96a699abd20512a2c887af634a84dd28
huskyhoochu <dfg1499@gmail.com>
2021-12-23 09:26:38 +0900

    로컬 스토리지에 테마 변경 값 저장

--- src/store/theme.ts
+++ src/store/theme.ts
@@ -1,11 +1,26 @@
-import { atom } from 'recoil';
+import { atom, AtomEffect } from 'recoil';
+
+const localStorageEffect: <T>(key: string) => AtomEffect<T> =
+  key =>
+  ({ setSelf, onSet }) => {
+    const savedValue = localStorage.getItem(key);
+    if (savedValue !== null) {
+      setSelf(JSON.parse(savedValue));
+    }
+
+    onSet(newValue => {
+      localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
 
 export const screenState = atom<ScreenType>({
   key: 'screenState',
   default: 'light',
+  effects_UNSTABLE: [localStorageEffect<ScreenType>('screen')],
 });
 
 export const themeState = atom<ThemeType>({
   key: 'themeState',
   default: 'beige',
+  effects_UNSTABLE: [localStorageEffect<ThemeType>('theme')],
 });
