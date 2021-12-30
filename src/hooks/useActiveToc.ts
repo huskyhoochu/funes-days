600617fa6985c714f9fec3d22367dd7a847e923f
huskyhoochu <dfg1499@gmail.com>
2021-12-30 13:58:08 +0900

    activeToc 관련 코드를 hook으로 분리

---
+++ src/hooks/useActiveToc.ts
@@ -0,0 +1,101 @@
+import { useEffect, useState } from 'react';
+
+const useActiveToc = (
+  mdContentRef: React.RefObject<HTMLDivElement>,
+  tocRef: React.RefObject<HTMLDivElement>,
+) => {
+  const [curScroll, setCurScroll] = useState<number>(0);
+
+  useEffect(() => {
+    const getCurScroll = (e: Event) => {
+      const curScroll = (e.target as Document).documentElement.scrollTop;
+      setCurScroll(curScroll);
+    };
+
+    const headings = Array.from(
+      mdContentRef.current?.children as HTMLCollection,
+    ).filter(el => el instanceof HTMLHeadingElement) as HTMLHeadingElement[];
+    const navArray = Array.from(
+      tocRef.current?.children[0].children as HTMLCollection,
+    );
+
+    const getDecodedHash = (el: Element, replaceText: string): string => {
+      let decodedHash = '';
+      if (el instanceof HTMLAnchorElement) {
+        decodedHash = decodeURIComponent((el as HTMLAnchorElement)?.hash);
+      } else if (
+        el.children.length > 1 &&
+        el.children[1] instanceof HTMLUListElement
+      ) {
+        const nestedArr = Array.from(el.children[1].children as HTMLCollection);
+        toggleActiveNav(nestedArr, replaceText);
+        decodedHash = getDecodedHash(el.children[0], replaceText);
+      } else if (
+        el.children.length === 1 &&
+        el.children[0] instanceof HTMLUListElement
+      ) {
+        const nestedArr = Array.from(el.children[0].children as HTMLCollection);
+        toggleActiveNav(nestedArr, replaceText);
+      } else {
+        decodedHash = getDecodedHash(el.children[0], replaceText);
+      }
+      return decodedHash;
+    };
+
+    const getActiveNav = (
+      navArr: Element[],
+      replacedText: string,
+    ): Element | undefined =>
+      navArr
+        .filter(nav => getDecodedHash(nav, replacedText) === replacedText)
+        .pop();
+
+    const getInactiveNavArr = (navArr: Element[], replacedText: string) =>
+      navArr.filter(nav => getDecodedHash(nav, replacedText) !== replacedText);
+
+    const toggleActiveNav = (navArr: Element[], replacedText: string) => {
+      getActiveNav(navArr, replacedText)?.classList?.add('active');
+      getInactiveNavArr(navArr, replacedText).forEach(nav =>
+        (nav as HTMLLIElement)?.classList?.remove('active'),
+      );
+    };
+
+    const inactiveAllNav = (navArr: Element[]) => {
+      navArr.forEach(nav =>
+        (nav as HTMLLIElement)?.classList?.remove('active'),
+      );
+    };
+
+    headings.forEach((heading, idx, arr) => {
+      const text = heading.innerText;
+      const nextHeadingTop =
+        arr[idx + 1]?.offsetTop || document.documentElement.offsetHeight;
+      const replacedText =
+        '#' +
+        text
+          .replace(/\s/g, '-')
+          .replace(/[^가-힣0-9a-zA-Z-]/g, '')
+          .toLowerCase();
+
+      // 스크롤이 해당 헤딩에 진입했을 때
+      // 해당 nav를 active한다
+      if (curScroll >= heading.offsetTop - 1 && curScroll < nextHeadingTop) {
+        toggleActiveNav(navArray, replacedText);
+      }
+
+      // 스크롤이 전체 컨텐츠보다 상위에 있을 때
+      // 모든 nav를 inactive 시킨다
+      if (curScroll < arr[0].offsetTop - 1) {
+        inactiveAllNav(navArray);
+      }
+    });
+
+    window.addEventListener('scroll', getCurScroll);
+
+    return () => {
+      window.removeEventListener('scroll', getCurScroll);
+    };
+  }, [curScroll, setCurScroll]);
+};
+
+export default useActiveToc;
--- src/pages/dev/{MarkdownRemark.frontmatter__slug}.tsx
+++ src/pages/dev/{MarkdownRemark.frontmatter__slug}.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useRef } from 'react';
 import dayjs from 'dayjs';
 import { graphql } from 'gatsby';
 import PostLayout from '@/layout/post';
 import { NarrowContainerWrapper } from '@/styles/container';
 import { MarkdownWrapper } from '@/components/pages/dev/styled';
 import useTheme from '@/hooks/useTheme';
+import useActiveToc from '@/hooks/useActiveToc';
 
 interface Props {
   data: {
@@ -27,96 +28,9 @@ const DevTemplate: React.FC<Props> = ({ data }) => {
   const [, screen] = useTheme();
   const { markdownRemark } = data;
   const { frontmatter, html, timeToRead, tableOfContents } = markdownRemark;
-  const [curScroll, setCurScroll] = useState<number>(0);
-
-  useEffect(() => {
-    const getCurScroll = (e: Event) => {
-      const curScroll = (e.target as Document).documentElement.scrollTop;
-      setCurScroll(curScroll);
-    };
-
-    const mdContent = document.getElementsByClassName('md-content')[0];
-    const headings = Array.from(mdContent.children).filter(
-      el => el instanceof HTMLHeadingElement,
-    ) as HTMLHeadingElement[];
-    const toc = document.getElementById('toc');
-    const navList = toc?.children[0].children as HTMLCollection;
-    const navArray = Array.from(navList);
-
-    headings.forEach((heading, idx, arr) => {
-      const top = heading.offsetTop;
-      const text = heading.innerText;
-      const nextHeadingTop =
-        arr[idx + 1]?.offsetTop || document.documentElement.offsetHeight;
-      const replacedText =
-        '#' +
-        text
-          .replace(/\s/g, '-')
-          .replace(/[^가-힣0-9a-zA-Z-]/g, '')
-          .toLowerCase();
-
-      const getDecodedHash = (el: Element): string => {
-        let decodedHash;
-        if (el instanceof HTMLAnchorElement) {
-          decodedHash = decodeURIComponent((el as HTMLAnchorElement)?.hash);
-        } else if (
-          el.children.length > 1 &&
-          el.children[1] instanceof HTMLUListElement
-        ) {
-          const nestedArr = Array.from(
-            el.children[1].children as HTMLCollection,
-          );
-          toggleActive(nestedArr);
-          decodedHash = getDecodedHash(el.children[0]);
-        } else if (
-          el.children.length === 1 &&
-          el.children[0] instanceof HTMLUListElement
-        ) {
-          const nestedArr = Array.from(
-            el.children[0].children as HTMLCollection,
-          );
-          toggleActive(nestedArr);
-          decodedHash = '';
-        } else {
-          decodedHash = getDecodedHash(el.children[0]);
-        }
-        return decodedHash;
-      };
-
-      const toggleActive = (navArr: Element[]) => {
-        if (curScroll >= top - 1 && curScroll < nextHeadingTop) {
-          const activeNav = navArr
-            .filter(nav => getDecodedHash(nav) === replacedText)
-            .pop();
-
-          (activeNav as HTMLLIElement)?.classList?.add('active');
-
-          const inactiveNav = navArr.filter(
-            nav => getDecodedHash(nav) !== replacedText,
-          );
-
-          inactiveNav.forEach(nav =>
-            (nav as HTMLLIElement)?.classList?.remove('active'),
-          );
-        }
-
-        // 전체 텍스트보다 위로 벗어났을 경우
-        if (curScroll < arr[0].offsetTop - 1) {
-          navArr.forEach(nav =>
-            (nav as HTMLLIElement)?.classList?.remove('active'),
-          );
-        }
-      };
-
-      toggleActive(navArray);
-    });
-
-    window.addEventListener('scroll', getCurScroll);
-
-    return () => {
-      window.removeEventListener('scroll', getCurScroll);
-    };
-  }, [curScroll, setCurScroll]);
+  const mdContentRef = useRef<HTMLDivElement>(null);
+  const tocRef = useRef<HTMLDivElement>(null);
+  useActiveToc(mdContentRef, tocRef);
 
   return (
     <PostLayout title={frontmatter.title}>
@@ -137,13 +51,17 @@ const DevTemplate: React.FC<Props> = ({ data }) => {
             </div>
           </div>
           <div
+            ref={mdContentRef}
             className="md-content"
             dangerouslySetInnerHTML={{ __html: html }}
           />
         </NarrowContainerWrapper>
         <div className="toc-group">
           <h5>Table of Contents</h5>
-          <div id="toc" dangerouslySetInnerHTML={{ __html: tableOfContents }} />
+          <div
+            ref={tocRef}
+            dangerouslySetInnerHTML={{ __html: tableOfContents }}
+          />
         </div>
       </MarkdownWrapper>
     </PostLayout>
