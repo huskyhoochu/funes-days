6cfb5b6cea14d3d577ac06ee972803ee42c12f3c
huskyhoochu <dfg1499@gmail.com>
2022-03-30 18:31:44 +0900

    dev 목록 페이지 스타일링

--- src/templates/devPosts/index.tsx
+++ src/templates/devPosts/index.tsx
@@ -5,6 +5,7 @@ import HomeLayout from '@/layout/home';
 import Header from '@/components/atoms/header';
 import { DevListWrapper } from './styled';
 import Pagination from '@/components/templates/pagination';
+import useTheme from '@/hooks/useTheme';
 
 interface Props {
   data: {
@@ -29,11 +30,24 @@ interface Props {
 }
 
 const DevList: React.FC<Props> = ({ data }) => {
+  const [, screen] = useTheme();
   const { edges, pageInfo } = data.allMarkdownRemark;
   return (
     <HomeLayout title="Dev Posts">
       <Header backgroundColor="transparent" />
-      <DevListWrapper>
+      <div
+        style={{
+          position: 'fixed',
+          inset: 0,
+          backgroundImage:
+            'url("https://images.unsplash.com/photo-1634247030997-6ea23b24ba63?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80")',
+          zIndex: 0,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      ></div>
+      <DevListWrapper screen={screen}>
         <div className="title-group">
           <h3>Dev Posts</h3>
         </div>
--- src/templates/devPosts/styled.ts
+++ src/templates/devPosts/styled.ts
@@ -1,9 +1,27 @@
 import styled from '@emotion/styled';
 import { breakPoints } from '@/styles/screen';
 import { H4Class } from '@/styles/typography';
+import { css } from '@emotion/react';
 
-export const DevListWrapper = styled.div`
+export const DevListWrapper = styled.div<{
+  screen: ScreenType;
+}>`
   padding-block: 120px;
+  position: relative;
+  z-index: 0;
+  background-color: transparent;
+
+  ${props =>
+    props.screen === 'light' &&
+    css`
+      backdrop-filter: blur(6px) contrast(40%);
+    `}
+
+  ${props =>
+    props.screen === 'dark' &&
+    css`
+      backdrop-filter: blur(6px) brightness(0.4);
+    `}
 
   .title-group {
     h3 {
